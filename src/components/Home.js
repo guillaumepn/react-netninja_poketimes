98ad682e085d8561af0357bd9c14622616da98e1
guillaumepn <guillaumepn@free.fr>
2019-08-17 13:52:59 +0200

    Add test HOC, axios, handle API call with useEffect hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nobis alias dolores dolor? Hic eos iusto ea quis dolorum odit vitae pariatur, omnis nulla adipisci ab corporis earum reprehenderit id.</p>
-        </div>
-    );
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(response => {
+            setPosts(
+                response.data
+            );
+        })
+        .catch(err => console.error(err));
+      return () => {};
+    }, [])
+
+    const postList = posts.length > 0 ? (
+        posts.map(post => (
+            <div className="post card" key={post.id}>
+                <div className="card-content">
+                    <span className="card-title">{post.title}</span>
+                    <p>{post.body}</p>
+                </div>
+            </div>
+        ))
+    ) : (
+        <div className="center">No posts here</div>
+    )
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <div className="list">
+        {postList}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
